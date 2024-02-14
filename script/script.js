83987e3fde52cecf22bf117f1fcbd6dc473a1854
Lins <110426000+Lins-T@users.noreply.github.com>
2024-02-14 01:07:58 +0000

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,30 @@
+'use strict';
+
+const bars = document.querySelectorAll('.bar');
+const body = document.querySelector('body');
+let barChart = Array.from(bars);
+
+async function js() {
+  let data = await fetch('../data.json');
+  let jsonData = await data.json();
+
+ await barChart.forEach(bar => {
+
+    let amntTip = document.createElement('div');
+    amntTip.classList.add('tooltip');
+
+    bar.addEventListener('click', btn => {
+      let txtData = jsonData[barChart.indexOf(bar)].amount;
+
+      amntTip.innerHTML = '$' + txtData;
+      bar.appendChild(amntTip);
+
+      amntTip.classList.toggle('show')
+
+    }, false);
+
+  })
+}
+
+js()
 
